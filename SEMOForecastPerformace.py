b7ea383b03a4afd9fb5fdc0b2b09f2c08b306405
eddiephelanfortyfive <23397179@studentmail.ul.ie>
2024-06-27 09:44:24 +0100

    Fixed Outturn

--- SEMOForecastPerformace.py
+++ SEMOForecastPerformace.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-startDate = '2024-06-10'
+startDate = '2024-06-20'
 endDate = '2024-06-26'
 FileName = "PUB_DailyLoadFcst_202406260835.xml" #5 min period isolation example
 PageSize = '10'
@@ -9,41 +9,44 @@ SortBy = 'PublishTime'
 ForecastReportName = 'Daily Load Forecast Summary'
 OutturnReportName = 'Average Outturn Availability'
 
-dateRange = pd.date_range(start=startDate, end=endDate)
+dateRange = pd.date_range(start=startDate, end=endDate).date
 
+api_link = "http://reports.sem-o.com/api/v1/documents/static-reports"
+
+#Retriving Daily load forecast Summary
 for date in dateRange:
-
-    ForecastParameters = {
+     
+     ForecastParameters = {
         'ReportName' : ForecastReportName,
-        'Date': date.date(),
+        'Date': date,
         'page_size': PageSize,
         'sort_by': SortBy
     }
+     ForecastResponse = requests.get(api_link, params=ForecastParameters)
+     if ForecastResponse.status_code != 200:
+        print(f"Failed to retrieve data: {ForecastResponse.status_code}")
+         
+     else:
+        ForecastGridInfo = ForecastResponse.json()
+
+        print("\n\n\nForecast Grid Information:", ForecastGridInfo)
+         
+
+#Retriving the Average Outturn Availability 
+for date in dateRange:
 
     OutturnParameters = {
         'ReportName' : OutturnReportName,
-        'Date': date.date(),
+        'Date': date,
         'page_size': PageSize,
         'sort_by': SortBy
     }
-    api_link = "http://reports.sem-o.com/api/v1/documents/static-reports"
-
-    ForecastResponse = requests.get(api_link, params=ForecastParameters)
     OutturnResponse = requests.get(api_link, params=OutturnParameters)
-
-    if ForecastResponse.status_code != 200:
-        print(f"Failed to retrieve data: {ForecastResponse.status_code}")
-
-    elif OutturnResponse.status_code != 200:
+    if OutturnResponse.status_code != 200:
         print(f"Failed to retrieve data: {OutturnResponse.status_code}")
 
     else:
-        ForecastGridInfo = ForecastResponse.json()
         OutturnGridInfo = OutturnResponse.json()
 
-        print("\n\n\nForecast Grid Information:", ForecastGridInfo)
+        print("Forecast Grid Information:", ForecastGridInfo)
         print("\n\n\nOutturn Grid Information:", OutturnGridInfo)
-
-
-
-
